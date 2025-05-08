81a8e97740e94291c1a85fb6475622d77db20cf5
Marc-Hab <marchelinohabchi@gmail.com>
2025-05-07 20:18:18 -0400

    Removed frontend files to solely focus on backend

--- frontend/app/constants.tsx
+++
@@ -1,2 +0,0 @@
-// constants.tsx
-export const GOOGLE_MAPS_APIKEY = ""; //YOUR_GOOGLE_API_KEY_HERE
--- frontend/app/index.tsx
+++
@@ -1,9 +0,0 @@
-import { StyleSheet, Text, View, TextInput } from "react-native";
-
-export default function Index() {
-  return (
-    <View>
-      <Text>This is the landing page of the app</Text>
-    </View>
-  );
-}
